0045e0d3565bf631c5c3a92fafb24deff1701def
ktlo <ktlo@handtruth.com>
2021-08-01 20:15:27 +0300

    Python python python

--- src/main/java/li/cil/oc2/common/bus/device/data/PythonBlockDeviceData.java
+++ src/main/java/li/cil/oc2/common/bus/device/data/PythonBlockDeviceData.java
@@ -2,8 +2,6 @@ package li.cil.oc2.common.bus.device.data;
 
 import li.cil.oc2.api.bus.device.data.BlockDeviceData;
 import li.cil.sedna.api.device.BlockDevice;
-import li.cil.sedna.buildroot.Buildroot;
-import li.cil.sedna.buildroot.Python;
 import li.cil.sedna.device.block.ByteBufferBlockDevice;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
@@ -12,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public final class PythonBlockDeviceData extends ForgeRegistryEntry<BlockDeviceData> implements BlockDeviceData {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -20,10 +19,19 @@ public final class PythonBlockDeviceData extends ForgeRegistryEntry<BlockDeviceD
 
     private static final ByteBufferBlockDevice INSTANCE;
 
+    private static InputStream getRootFilesystem() {
+        InputStream stream = PythonBlockDeviceData.class.getClassLoader()
+                .getResourceAsStream("/images/python.ext2");
+        if (stream == null) {
+            throw new IllegalStateException("no python image found");
+        }
+        return stream;
+    }
+
     static {
         ByteBufferBlockDevice instance;
         try {
-            instance = ByteBufferBlockDevice.createFromStream(Python.getRootFilesystem(), true);
+            instance = ByteBufferBlockDevice.createFromStream(getRootFilesystem(), true);
         } catch (final IOException e) {
             LOGGER.error(e);
             instance = ByteBufferBlockDevice.create(0, true);
